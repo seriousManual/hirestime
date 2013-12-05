752a21360a062682e70274163d760393dd63e550
zaphod1984 <zaphod84@gmx.de>
2013-12-05 11:18:12 +0100

    added time units

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+var S = 'seconds';
+var MS = 'milliseconds';
+var NS = 'nanoseconds';
+
 function hirestime() {
     var start = process.hrtime();
 
-    return function() {
+    return function(unit) {
         var elapsed = process.hrtime(start);
 
-        return elapsed[0] * 1e3 + elapsed[1] / 1e6;
+        return recalc(elapsed[0] * 1e3 + elapsed[1] / 1e6, unit);
     }
 }
+
+function recalc(msTime, unit) {
+    var time;
+    if(!unit || unit === MS)  time = msTime;
+    if(unit === S) time = msTime / 1000;
+    if(unit === NS) time = msTime * 1000;
+
+    return parseInt(time * 100, 10) / 100;
+}
+
+hirestime.S = S;
+hirestime.MS = MS;
+hirestime.NS = NS;
 
 module.exports = hirestime;
--- tests/tests.js
+++ tests/tests.js
@@ -4,14 +4,44 @@ var hirestime = require('../');
 
 describe('hirestime', function() {
 
-    it('should return an approximate number of elapsed milliseconds', function(done) {
+    it('should return an approximate number of elapsed time in milliseconds (no unit given)', function(done) {
         var getElapsed = hirestime();
 
         setTimeout(function() {
-            expect(getElapsed()).to.be.within(990, 1100);
+            expect(getElapsed()).to.be.within(90, 110);
 
             done();
-        }, 1000);
+        }, 100);
+    });
+
+    it('should return an approximate number of elapsed time in seconds (seconds unit)', function(done) {
+        var getElapsed = hirestime();
+
+        setTimeout(function() {
+            expect(getElapsed(hirestime.S)).to.be.within(0.09, 0.11);
+
+            done();
+        }, 100);
+    });
+
+    it('should return an approximate number of elapsed time in seconds (milliseconds unit)', function(done) {
+        var getElapsed = hirestime();
+
+        setTimeout(function() {
+            expect(getElapsed(hirestime.MS)).to.be.within(90, 110);
+
+            done();
+        }, 100);
+    });
+
+    it('should return an approximate number of elapsed time in seconds (nanoseconds unit)', function(done) {
+        var getElapsed = hirestime();
+
+        setTimeout(function() {
+            expect(getElapsed(hirestime.NS)).to.be.within(90000, 110000);
+
+            done();
+        }, 100);
     });
 
 });
