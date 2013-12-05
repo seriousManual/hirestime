841dfde55a4a9c988ea314ff9213a57739984193
zaphod1984 <zaphod84@gmx.de>
2013-12-05 12:00:20 +0100

    added hrtimeMock

--- tests/tests.js
+++ tests/tests.js
@@ -1,29 +1,7 @@
 var expect = require('chai').expect;
+var hrtimeMock = require('hrtimemock');
 
 var hirestime = require('../');
-
-function hrtimeMock(msTime) {
-    var _hrtime = process.hrtime;
-
-    var returnValue = [
-        parseInt(msTime / 1e3, 10),
-        (msTime % 1000) * 1e6
-    ];
-
-    var isFirst = true;
-    process.hrtime = function() {
-        if(isFirst) {
-            isFirst = false;
-
-            return [];
-        } else {
-            isFirst = true;
-            process.hrtime = _hrtime;
-
-            return returnValue;
-        }
-    }
-}
 
 describe('hirestime', function() {
 
