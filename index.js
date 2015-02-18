203e6ed9fbcf5b78537e8847f65699937c6f992f
zaphod1984 <zaphod84@gmx.de>
2015-02-18 10:12:59 +0100

    cosmetics

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var S = 'seconds';
-var MS = 'milliseconds';
-var NS = 'nanoseconds';
+var S = 's';
+var MS = 'ms';
+var NS = 'ns';
 
 function hirestime() {
     var start = process.hrtime();
 
-    return function(unit) {
+    return function (unit) {
         var elapsed = process.hrtime(start);
 
         var time;
-        if(unit === S) time = elapsed[0] + elapsed[1] / 1e9;
-        else if(unit === NS) time = elapsed[0] * 1e9 + elapsed[1];
-        else if(unit === MS || !unit || !time) time = elapsed[0] * 1e3 + elapsed[1] / 1e6;
+        if (unit === S) time = elapsed[0] + elapsed[1] / 1e9;
+        else if (unit === NS) time = elapsed[0] * 1e9 + elapsed[1];
+        else if (unit === MS || !unit || !time) time = elapsed[0] * 1e3 + elapsed[1] / 1e6;
 
         return parseInt(time * 100, 10) / 100;
     }
